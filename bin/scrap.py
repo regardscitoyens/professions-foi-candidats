b84b7a380d4a6e2b3889814f35389147f14103f8
RouxRC <b.ooghe@gmail.com>
2020-07-08 10:31:28 +0200

    add code for yet another version of the website for municipales 2020

--- bin/scrap.py
+++ bin/scrap.py
@@ -8,6 +8,8 @@ import requests
 
 HOSTURL = 'https://programme-candidats.interieur.gouv.fr/'
 DATAURL = HOSTURL + 'data-jsons/'
+DATAURL2 = HOSTURL + 'ajax/data/'
+PDFSURL = HOSTURL + "data-pdf-propagandes/"
 
 def downloadPDF(eldir, filename, url, retries=3):
     filepath = os.path.join(eldir, "%s.pdf" % filename)
@@ -50,7 +52,7 @@ def collect_regionales(elcode="RG15"):
                     downloadPDF(eldir, codeId + 'profession_foi', HOSTURL + liste['propagande'])
 
 def request_data(url, field, fallback_field=None, retries=10):
-    jsonurl = "%s.json" % url
+    jsonurl = "%s.json?_=%s" % (url, time.time())
     try:
         jsondata = requests.get(jsonurl).json()
         if field in jsondata:
@@ -157,12 +159,66 @@ def scrape_election(el):
     if nb_n:
         print "%s: %s new documents collected (%s total candidates are published out of %s listed in %s %s and %s %s)." % (el["name"].encode("utf-8"), nb_n, nb_d, nb_c, nb_g, el["granu"], nb_g2, el["granu2"])
 
+
+def scrape_municipales(elcode="MN20"):
+    eldir = os.path.join("documents", elcode)
+    if not os.path.exists(eldir):
+        os.makedirs(eldir)
+    nb_dep = 0
+    nb_com = 0
+    nb_c = 0
+    nb_d = 0
+    nb_n = 0
+    url = DATAURL2 + "departements"
+    data = {}
+    for dept in request_data(url, "departements"):
+        nb_dep += 1
+        depcode = dept["id"]
+        depname = dept["name"]
+        depurl = DATAURL2 + "communes_%s" % depcode
+        data[depcode] = {
+            "name": depname,
+            "url": depurl,
+            "communes": {}
+        }
+        deptdir = os.path.join(eldir, depcode)
+        if not os.path.exists(deptdir):
+            os.makedirs(deptdir)
+        for commune in request_data(depurl, "data"):
+            nb_com += 1
+            comcode = commune["id"]
+            comname = commune["name"]
+            comurl = DATAURL2 + "candidats_%s" % comcode
+            data[depcode]["communes"][comcode] = {
+                "name": comname,
+                "url": comurl,
+                "candidats": request_data(comurl, "data")
+            }
+            comdir = os.path.join(deptdir, comcode)
+            if not os.path.exists(comdir):
+                os.makedirs(comdir)
+            for candidat in data[depcode]["communes"][comcode]["candidats"]:
+                nb_c += 1
+                name = candidat["candidat"].split(',')[0].replace(' ', '_')
+                codeId = '%s-%s-%s-%s-%s%s-' % (elcode, depcode, comcode, name, candidat["num"], "2")
+                if candidat['pdf'] != "0":
+                    nb_d += 1
+                    nb_n += downloadPDF(comdir, codeId + 'profession_foi', PDFSURL + "%s.pdf" % candidat['pdf'])
+
+    with open(os.path.join(eldir, "%s%s-metadata.json" % (elcode, "2")), "w") as f:
+        json.dump(data, f, indent=2)
+    if nb_n:
+        print "%s: %s new documents collected (%s total candidates are published out of %s listed in %s departments and %s communes)." % (elcode, nb_n, nb_d, nb_c, nb_dep, nb_com)
+
+
 if __name__ == '__main__':
     election = ""
     if len(sys.argv) > 1:
         election = sys.argv[1]
     if election == "RG15":
         collect_regionales()
+    elif election == "MN20":
+        scrape_municipales()
     else:
         for el in list_elections():
             if not election or election == el["code"]:
